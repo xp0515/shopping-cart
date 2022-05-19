c167a4bd027ad9dd5d8fddd94359b82f4eddcaae
xp0515 <x6868p@gmail.com>
2022-05-20 02:49:01 +1000

    initial commit

---
+++ apps/coding-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'peng-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'coding-test';
+}
